07197f1978ce7163cf5b67ea111d32cdc84894c7
Robert Graham <rjcgraham@gmail.com>
2018-12-05 14:57:59 -0800

    Add files via upload

---
+++ dotnetframework/servicelib/IEightBall.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+
+namespace MagicEightBallServiceLib
+{
+    [ServiceContract(Namespace = "http://MyCompany.com")]
+    public interface IEightBall
+    {
+        // Ask a question, receive an answer!
+        [OperationContract]
+        string ObtainAnswerToQuestion(string userQuestion);
+    }
+}
---
+++ dotnetframework/servicelib/MagicEightBallService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+// The key WCF namespace.
+
+namespace MagicEightBallServiceLib
+{
+    public class MagicEightBallService : IEightBall
+    {
+        public MagicEightBallService()
+        {
+            Console.WriteLine("The 8-Ball awaits your question...");
+        }
+        public string ObtainAnswerToQuestion(string userQuestion)
+        {
+            string[] answers =  { "Future Uncertain", "Yes", "No",
+        "Hazy", "Ask again later", "Definitely" };
+
+            // Return a random response.
+            Random r = new Random();
+            return answers[r.Next(answers.Length)];
+        }
+    }
+}
