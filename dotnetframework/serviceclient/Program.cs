f4cabf33fe2d0b19746e7fc4fdbb71b3ef62c585
Robert Graham <rjcgraham@gmail.com>
2018-12-05 14:58:50 -0800

    Add files via upload

---
+++ dotnetframework/serviceclient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MagicEightBallServiceClient.ServiceReference1;
+
+namespace MagicEightBallServiceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Ask the Magic 8 Ball *****\n");
+
+            using (EightBallClient ball = new EightBallClient("basicHttpBinding_IEightBall"))
+            {
+
+                Console.Write("Your question: ");
+                //string question = Console.ReadLine();
+                string question = "Will there be another fire?";
+                string answer =
+                    ball.ObtainAnswerToQuestion(question);
+                Console.WriteLine("In response to {0}   8-Ball says: {1}", question, answer);
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+}
